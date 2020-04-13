60c8b1914ed2f43c04ffe291500ded9e90c15597
sk259 <skhurshid@partners.org>
2020-04-12 23:16:25 -0400

    tweaks

--- accel.py
+++ accel.py
@@ -11,7 +11,7 @@ y_train = y_train.iloc[:,0]
 
 # model
 mod = Sequential()
-mod.add(Dense(256, activation='relu', input_dim=11230))
+mod.add(Dense(256, activation='relu', input_dim=112320))
 mod.add(Dense(256, activation='relu'))
 mod.add(Dense(256, activation='relu'))
 mod.add(Dense(256, activation='relu'))
@@ -23,4 +23,24 @@ mod.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 mod.fit(X_train.values,y_train,
+        epochs=50,batch_size=16)
+
+# input
+X_value = pd.read_csv('/mnt/ml4cvd/projects/skhurshid/bmi707/value.tsv',sep='\t',header=None)
+X_dm = pd.read_csv('/mnt/ml4cvd/projects/skhurshid/bmi707/dm.tsv',sep='\t',header=None)
+X_bmi = pd.read_csv('/mnt/ml4cvd/projects/skhurshid/bmi707/bmi.tsv',sep='\t',header=None)
+
+# model
+mod = Sequential()
+mod.add(Dense(256, activation='relu', input_dim=1))
+mod.add(Dense(256, activation='relu'))
+mod.add(Dense(256, activation='relu'))
+mod.add(Dense(1, activation='relu'))
+
+opt=Adam(lr=0.01)
+mod.compile(loss='mse',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+mod.fit(X_value.values,X_bmi.values,
         epochs=50,batch_size=16)
