feec7c111bbfe0b5dc415745720ec27ef4af9ca7
sk259 <skhurshid@partners.org>
2020-04-23 23:50:28 -0400

    script to run on cluster

---
+++ assignments/downsample.py
@@ -0,0 +1,38 @@
+# Dependencies
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Dense
+
+# Load data
+colnames = pd.array(range(1,112321)) # The number of time intervals in the database
+train_controls = pd.read_csv('/Volumes/medpop_afib/skhurshid/bmi707/train_controls_data.csv', names = colnames, header = None)
+train_cases = pd.read_csv('/Volumes/medpop_afib/skhurshid/bmi707/train_cases_data.csv', names = colnames, header = None)
+
+# Creating labels
+zeros = pd.Series(np.zeros(694))
+ones = pd.Series(np.ones(694))
+y_train = zeros.append(ones)
+
+# Combine cases and controls
+x_train = pd.concat([train_controls, train_cases])
+
+# Downsampling
+x_train_reduced = x_train.groupby(np.arange(len(x_train.columns))//32, axis=1).mean()
+
+# Re-shaping data
+x_train_reshaped = x_train.values.reshape(-1, 1, 3510)
+
+# Model
+model = Sequential()
+model.add(layers.LSTM(128, input_shape=(1,3510)))
+model.add(Dense(1),activation='sigmoid')
+
+# Compile
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# Fit
+model.fit(x_train_reshaped, y_train, epochs = 10, verbose = 1)
